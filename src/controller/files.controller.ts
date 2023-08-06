398c8c65430970ca76366bac5f721c1b48b03c44
anggiat0304 <anggiatpangaribuan28@gmail.com>
2023-08-06 19:25:44 +0700

    change

--- src/controller/files.controller.ts
+++ src/controller/files.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Res,Param, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Res,Param, HttpStatus, Query } from '@nestjs/common';
+import { Response, query } from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as JSZip from 'jszip';
+import * as archiver from 'archiver';
 
-@Controller('/api/files')
+@Controller('/api/file')
 export class FilesController {
-  @Get(':filename')
+  @Get('/download/:filename')
   async downloadFile(@Param('filename') namaFile: string, @Res() res: Response) {
-    const filePath = path.resolve(__dirname, '..', 'uploads', namaFile);
+    const filePath = path.resolve(__dirname, '..', '../src/folder/file', namaFile);
+   
 
     // Cek apakah file ada
     if (!fs.existsSync(filePath)) {
@@ -19,4 +22,32 @@ export class FilesController {
     res.setHeader('Content-Disposition', `attachment; filename=${namaFile}`);
     fs.createReadStream(filePath).pipe(res);
   }
+
+  @Get('/bulkdownload')
+  async bulkDownload(@Query() query:{ filename: string}, @Res() response: Response) {
+    const { filename } = query;
+    const filenames = Array.isArray(filename) ? filename : [filename];
+
+    const zip = new JSZip();
+
+    for (const filename of filenames) {
+      const filePath = path.resolve(__dirname, '..', '../src/folder/file', filename);
+      const fileData = fs.readFileSync(filePath);
+      zip.file(filename, fileData);
+    }
+
+    response.set({
+      'Content-Type': 'application/zip',
+      'Content-Disposition': `attachment; filename=files.zip`,
+    });
+
+    const archive = archiver('zip');
+    archive.pipe(response);
+    console.log(archive.pipe(response));
+    zip.generateNodeStream({ streamFiles: true })
+      .pipe(archive);
+
+    archive.finalize();
+  }
+
 }
